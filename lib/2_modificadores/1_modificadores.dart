94a459f59b4e43660eadb6a903f60799dc6f2791
Zorzan99 <solotovzds@hotmail.com>
2024-01-12 23:51:28 -0300

    Feat: criacao modificadores

---
+++ lib/2_modificadores/1_modificadores.dart
@@ -0,0 +1,30 @@
+void main() {
+  var nomeCompleto = "gabriel";
+
+  print(nomeCompleto);
+  nomeCompleto = "zorzan";
+  print(nomeCompleto);
+
+  // final
+
+  //VARIAVEIS FINAIS (NÃO PODEM SER ALTERADAS DEPOIS DE INICIALIZADAS,
+  //SÃO DEFINIDAS NO PROGRAMA EM TEMPO DE EXECUÇÃO (RUNTIME))
+  //UTILIZE SEEMM MODERAÇÃO!!!
+
+  final nomeCompletoFinal = "Gabriel zorzan";
+  final nomeCompletoFinal1 = nomeCompleto;
+
+  //const
+  //VARIAVEIS (NÃO PODEM SER ALTERADAS DEPOIS DE INICIALIZADAS,
+  //SÃO DEFINIDAS NO PROGRAMA EM TEMPO DE COMPPILAÇÃO,
+  //NÃO PODEM RECEBER VALORES DE OUTRAS VARIAVEIS A NAO SER QUE ESSAS VARIAVEIS FOREM CONST TAMBEM
+  //UTILIZE SEEEEM MODERACAO)
+
+  const nomeCompletoConst = "Gabriel Zorzan";
+  const nomeCompletoConst2 = nomeCompletoConst;
+}
+
+ 
+
+// dois momentos de na aplicacao
+//momento de compilacao quando o codigo esta sendo compilado para rodar
