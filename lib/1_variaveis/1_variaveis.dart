9593694caf2498dca86a002f33fd10334f6c2b97
Zorzan99 <solotovzds@hotmail.com>
2024-01-12 23:28:27 -0300

    Feat: finalizando modulo variaveis

---
+++ lib/1_variaveis/1_variaveis.dart
@@ -0,0 +1,18 @@
+void main() {
+  int idade = 25;
+
+  //quando colocamos var e iniciamos a variavel com um valor, ele ja inicia a mesma com o valor dela.
+  var idade2 = 27;
+
+  double valporIphone = 12.000;
+
+  String nomeTeste = "Teste";
+
+  // todas as classes herdam de objetc.
+  Object objetoQualquer = 1.2;
+
+  //aceita qualquer tipo igual o object
+  dynamic valorQualquer = "dynamic";
+
+  //EVITAR AO MAXIMO A UTILIZACAO DE OBJECT E DYNAMIC PARA DECLARAR VARIAVEIS
+}
